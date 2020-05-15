9ea87938c0aad141f7f42ffaf7799d5b0425ffe4
Valeria Meza <valeriameza173@gmail.com>
2020-05-14 19:17:29 -0500

    went over basic data types

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    char name [] = "Valeria";
-    int age = 24;
-    printf("Hello %s\n", name);
-    printf("%s is %d\n", name, age);
+//    char name [] = "Valeria";
+//    int age = 24;
+//    printf("Hello %s\n", name);
+//    printf("%s is %d\n", name, age);
+//
+//    age = 10; // changed value of age
+//    printf("%d",age);
 
-    age = 10;
-    printf("%d",age);
+    int age = 29;
+    double pi = 3.14159;
+    float decimal = 1.3f;
+    char letter = 'A'; // can only store one character
+    char sentence[] = "This is how to create a string of characters in C.";
     return 0;
 }
