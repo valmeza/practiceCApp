b972432084465e9b084b1ea4deb32b83332bc80f
Valeria Meza <valeriameza173@gmail.com>
2020-05-14 19:21:33 -0500

    minor change

--- main.c
+++ main.c
@@ -9,10 +9,11 @@ int main() {
 //    age = 10; // changed value of age
 //    printf("%d",age);
 
-    int age = 29;
-    double pi = 3.14159;
-    float decimal = 1.3f;
-    char letter = 'A'; // can only store one character
-    char sentence[] = "This is how to create a string of characters in C.";
+//    int age = 29;
+//    double pi = 3.14159;
+//    float decimal = 1.3f;
+//    char letter = 'A'; // can only store one character
+//    char sentence[] = "This is how to create a string of characters in C.";
+
     return 0;
 }
